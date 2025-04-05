ad4161f62be88800a262c18957113d66697d30fe
Jeremy <jeremy@dedicated-it.com>
2025-04-05 23:30:48 +0700

    add logging when create model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,12 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class Project extends Model
 {
     protected $table = 'projects';
 
     protected $guarded = [];
+
+    public static function boot() {
+        parent::boot();
+    
+        //once created/inserted successfully this method fired, so I tested foo 
+        static::created(function ($model) {
+            
+            $logMessage = "User with ID: " . Auth::id() . " Created Project Data";
+            Log::channel('userhistory')->info($logMessage, ['model' => $model]);
+        });
+
+        static::deleted(function ($model) {
+            $logMessage = "User with ID: " . Auth::id() . " Deleted Project Data";
+            Log::channel('userhistory')->info($logMessage, ['model' => $model]);
+        });
+    }
 
     /**
      * user relationship
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class Task extends Model
 {
@@ -12,6 +14,22 @@ class Task extends Model
     protected $guarded = [];
 
     protected $appends = ['project_name', 'created_date'];
+
+    public static function boot() {
+        parent::boot();
+    
+        //once created/inserted successfully this method fired, so I tested foo 
+        static::created(function ($model) {
+            $logMessage = "User with ID: " . Auth::id() . " Created Task Data";
+            Log::channel('userhistory')->info($logMessage, ['model' => $model]);
+        });
+
+        static::deleted(function ($model) {
+            $logMessage = "User with ID: " . Auth::id() . " Deleted Task Data";
+            Log::channel('userhistory')->info($logMessage, ['model' => $model]);
+        });
+    }
+    
 
     /**
      * user relationship
